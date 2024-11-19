e9cfefea63186873eac877a398fa368b59e79370
ErsleyDev <ersleyalamardid@gmail.com>
2024-11-19 12:04:50 +0100

    finDeSemana.py primer commit

--- Phyton2/phytonEjercicios/funciones/finDeSemana.py
+++ Phyton2/phytonEjercicios/funciones/finDeSemana.py
@@ -1,1 +1,8 @@
- 
+dia1 = input("Día: ")
+dia2 = input("Día: ")
+dia3 = input("Día: ")
+dias = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+findes = ["sabado", "domingo"]
+def finde(a, b, c):
+    if a == dias:
+        return 
