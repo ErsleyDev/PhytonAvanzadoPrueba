3a6474123a4000dc74f4956c0e220e2bac484d10
ErsleyDev <ersleyalamardid@gmail.com>
2024-12-03 14:37:08 +0100

    readWriteFile completado

--- Phyton2/phytonEjercicios/Ficheros/readWriteFile.py
+++ Phyton2/phytonEjercicios/Ficheros/readWriteFile.py
@@ -3,20 +3,26 @@ print("____________________")
 print("1. Introdudir datos")
 print("2. Leer datos existentes")
 print("3. Salir")
-opc = int(input("Introduce una opción: "))
-while opc != 3:
-    if opc == 1:
-        for i in range(1):
-            ruta = "/home/erschilon/Documentos/PhytonAvanzadoPrueba/Phyton2/phytonEjercicios/Ficheros/txtFiles/nombreCompleto.txt"
-            with open(ruta, mode="w", encoding="utf-8")as fichero:
-                name = input("Escribe tu nombre completo: ")
-                print(f"{name}", file=fichero)
 
+opc = int(input("Introduce una opción: "))
+while opc != 3: 
+    if opc <= 0 or opc >3:
+        print("ERROR")
+    else:
+        
+        if opc == 1:
+                ruta = "txtFiles/nombreCompleto.txt"
+                with open(ruta, mode="w", encoding="utf-8")as fichero:
+                    name = input("Escribe tu nombre completo: ")
+                    print(f"{name}", file=fichero)
                 fichero.close()
-while opc != 3:
-    if opc == 2:
-        ruta = "/home/erschilon/Documentos/PhytonAvanzadoPrueba/Phyton2/phytonEjercicios/Ficheros/txtFiles/nombreCompleto.txt"
-        with open(ruta, mode="r", encoding="utf-8")as fichero:
-            for line in fichero:
-                print(line)
-            fichero.close
+
+        elif opc == 2:
+            ruta = "txtFiles/nombreCompleto.txt"
+            with open(ruta, mode="r", encoding="utf-8")as fichero:
+                for line in fichero:
+                        print(line)
+                        fichero.close()
+    opc = int(input("Introduce una opción: "))
+
+print("ADIÓS")
