eaff073eed23ae8f60e0cfeb4a82039152eba533
ErsleyDev <ersleyalamardid@gmail.com>
2025-01-16 13:04:31 +0100

    ejerFinal v2.1

--- Phyton2/phytonEjercicios/Ficheros/pyEjercicoFinal/reglIESOrriols.py
+++ Phyton2/phytonEjercicios/Ficheros/pyEjercicoFinal/reglIESOrriols.py
@@ -1,3 +1,4 @@
+ruta = "pyEjercicoFinal/txt/IESOrriolsEst.txt"
 print("REGISTRO DEL ALUMNADO DEL IES ORRIOLS")
 print("****************************************")
 print()
@@ -11,19 +12,28 @@ def menu():
     print("4. ESTADÍSTICAS. mostrar la nota media de un alumno")
     print("5. SALIR")
     
-opc = 0
-while opc != 5:
-    menu()
-    opc = int(input("Elige la opcion: "))
-    if opc == 1:
+def entradaDatos(nia, nom, val, cas, ing, filo, hist):
         nia = int(input("NIA del alumno: "))
         nom = input("Nombre y primer apellido del alumno: ")
         print("Introduce las notas de Valenciano, Castellano, Inglés, Filosofía e Historia")
+        lnomasig=["Valenciano", "Castellano", "Inglés", "Filosofía", "Historia"]
+        for i in range(5):
+             
         val = int(input("Nota de Valenciano: "))
         cas = int(input("Nota de Castellano: "))
         ing = int(input("Nota de Inglés: "))
         filo = int(input("Nota de Filosofía: "))
         hist = int(input("Nota de HIstoria: "))
         notas = [val, cas, ing, filo, hist]
-print("hasta luego")
 
+        with open(ruta, mode="a", encoding="utf-8") as fichero:
+            print(f"{nia}, {nom}, {val}, {cas}, {ing}, {filo}, {hist}", file=fichero)
+
+opc = 0
+while opc != 5:
+    menu()
+    opc = int(input("Elige la opcion: "))
+    if opc == 1:
+        
+        infoAlum = entradaDatos(nia, nom, val, cas, ing, filo, hist)
+print("hasta luego")
