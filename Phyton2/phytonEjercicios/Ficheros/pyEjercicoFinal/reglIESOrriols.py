617beb9c1dd6eccb69bef65872fa6f835c5dac59
ErsleyDev <ersleyalamardid@gmail.com>
2025-01-16 15:06:18 +0100

    ejerFinal v2.2.1 opcion 2 añadida y en trabajo la funcion 3

--- Phyton2/phytonEjercicios/Ficheros/pyEjercicoFinal/reglIESOrriols.py
+++ Phyton2/phytonEjercicios/Ficheros/pyEjercicoFinal/reglIESOrriols.py
@@ -12,28 +12,42 @@ def menu():
     print("4. ESTADÍSTICAS. mostrar la nota media de un alumno")
     print("5. SALIR")
     
-def entradaDatos(nia, nom, val, cas, ing, filo, hist):
+def entradaDatos():
         nia = int(input("NIA del alumno: "))
         nom = input("Nombre y primer apellido del alumno: ")
         print("Introduce las notas de Valenciano, Castellano, Inglés, Filosofía e Historia")
         lnomasig=["Valenciano", "Castellano", "Inglés", "Filosofía", "Historia"]
+        lnotas = []
         for i in range(5):
-        
-        val = int(input("Nota de Valenciano: "))
-        cas = int(input("Nota de Castellano: "))
-        ing = int(input("Nota de Inglés: "))
-        filo = int(input("Nota de Filosofía: "))
-        hist = int(input("Nota de HIstoria: "))
-        notas = [val, cas, ing, filo, hist]
-
+            notas = int(input(f"Introduce la nota de {lnomasig[i]}: "))
+            lnotas.append(notas)
         with open(ruta, mode="a", encoding="utf-8") as fichero:
-            print(f"{nia}, {nom}, {val}, {cas}, {ing}, {filo}, {hist}", file=fichero)
+            print(f"{nia} {nom} {lnotas}", file=fichero)
+
+def mostrarDatos():
+    with open(ruta, mode="r", encoding="utf-8") as fichero:
+        for line in fichero:
+            print(line)
+
+def devolverLineaDatosAl():
+    nia = int(input("Introduce el NIA que deseas buscar"))
+    with open(ruta, mode="r", encoding="utf-8") as fichero:
+        for line in fichero:
+            
+
 
 opc = 0
 while opc != 5:
     menu()
     opc = int(input("Elige la opcion: "))
     if opc == 1:
+        entradaDatos()
+    elif opc == 2:
+        print(" ")
+        print("DATOS DEL ALUMNADO")
+        mostrarDatos()
+    elif opc == 3:
+        devolverLineaDatosAl()
         
-        infoAlum = entradaDatos(nia, nom, val, cas, ing, filo, hist)
-print("hasta luego")
+        
+print("Hasta luego")
